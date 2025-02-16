f54646c35c0ce2ea48b18a252018e3f3098dfb80
Moon <zxdev-git@outlook.com>
2025-02-16 03:45:08 +0000

    Update Obf.bat

--- src/Obf.bat
+++ src/Obf.bat
@@ -1,9 +1,9 @@
 @echo off
 echo.
-start files\README.txt
 if exist files\Read.true (
     set "Obfuscate=true"
 ) else (
+    start files\README.txt
     echo. >>files\Read.true
 )
 if "%Obfuscate%" EQU "true" ( files\lua\lua54.exe files\Prometheus\cli.lua --preset Strong --nocolors --LuaU script.lua )
